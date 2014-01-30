2b9b1fe11551ee35f8b5143f816313ea15946e44
Sergey Kosarevsky <sk@linderdaum.com>
2014-01-30 18:28:22 +0400

    Disable LibSSH2 on Win64 builds

--- wcm/libconf_win32.h
+++ wcm/libconf_win32.h
@@ -1,4 +1,10 @@
 #ifndef LIBCONF_WIN32_H
-#define LIBCONF_WIN32_H
-#define LIBSSH2_EXIST
+#	define LIBCONF_WIN32_H
+#	define LIBSSH2_EXIST
+
+#	if defined(_WIN64)
+//		Disable LibSSH2 on Win64 builds
+#		undef LIBSSH2_EXIST
+#	endif // _WIN64
+
 #endif
