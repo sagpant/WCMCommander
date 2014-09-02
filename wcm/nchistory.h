25d070366031731683bffc063481d75e8749f2a9
Sergey Kosarevsky <sk@linderdaum.com>
2014-09-02 19:29:15 +0400

    Refactored NCHistory

--- wcm/nchistory.h
+++ wcm/nchistory.h
@@ -12,50 +12,21 @@ using namespace wal;
 
 class NCHistory
 {
-	ccollect<std::vector<unicode_t> > list;
-	int current;
+	ccollect< std::vector<unicode_t> > m_List;
+	int m_Current;
+
 public:
-	NCHistory(): current( 0 ) {}
+	NCHistory(): m_List(), m_Current( 0 ) {}
 
-	void Clear()
-	{
-		list.clear();
-	}
+	void Clear();
+	void Put( const unicode_t* str );
+	void DeleteAll( const unicode_t* Str );
 
-	void Put( const unicode_t* str )
-	{
-		current = 0;
+	int Count() const { return m_List.count(); }
+	const unicode_t* operator[] ( int n ) { return n >= 0 && n < m_List.count() ? m_List[n].data() : 0; }
 
-		for ( int i = 0; i < list.count(); i++ )
-		{
-			const unicode_t* s;
-			const unicode_t* t;
-
-			for ( s = str, t = list[i].data(); *t && *s; s++, t++ )
-				if ( *t != *s ) { break; }
-
-			if ( *t == *s )
-			{
-				std::vector<unicode_t> p = list[i];
-				list.del( i );
-				list.insert( 0, p );
-				return;
-			}
-		}
-
-		const size_t HistorySize = 1000;
-
-		if ( list.count() > HistorySize ) { list.del( list.count() - 1 ); }
-
-		list.insert( 0, new_unicode_str( str ) );
-	}
-
-	int Count() const { return list.count(); }
-	const unicode_t* operator[] ( int n ) { return n >= 0 && n < list.count() ? list[n].data() : 0; }
-
-
-	unicode_t* Prev() { return ( current < 0 || current >= list.count() ) ? 0 : list[current++].data(); }
-	unicode_t* Next() { return ( current <= 0 || current > list.count() ) ? 0 : list[--current].data(); }
+	unicode_t* Prev() { return ( m_Current < 0 || m_Current >= m_List.count() ) ? 0 : m_List[m_Current++].data(); }
+	unicode_t* Next() { return ( m_Current <= 0 || m_Current > m_List.count() ) ? 0 : m_List[--m_Current].data(); }
 
 	~NCHistory() {}
 };
