b0b13b1fa5c23d317382be9778cc41cc3029df1a
Sergey Kosarevsky <sk@linderdaum.com>
2014-09-05 05:01:12 +0400

    OSX: use FreeType

--- wcm/libconf.h
+++ wcm/libconf.h
@@ -1,10 +1,12 @@
 #ifndef LIBCONF_H
 #define LIBCONF_H
 
-#ifdef _WIN32
-#include "libconf_win32.h"
+#if defined( _WIN32 )
+#	include "libconf_win32.h"
+#elif defined( __APPLE__ )
+#	include "libconf_osx.h"
 #else
-#include "libconf_ux.h"
+#	include "libconf_ux.h"
 #endif
 
 #endif
---
+++ wcm/libconf_osx.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define USEFREETYPE 1
