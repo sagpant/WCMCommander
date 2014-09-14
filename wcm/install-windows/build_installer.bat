9b9b0a153dc26f0db4f318a18879ab72a4d04de1
Sergey Kosarevsky <sk@linderdaum.com>
2014-09-15 00:22:54 +0400

    Added 64-bit installer

--- wcm/install-windows/build_installer.bat
+++ wcm/install-windows/build_installer.bat
@@ -5,12 +5,12 @@ rem Run this from 'git.master' folder
 rem
 
 
-rem Prepare binaries
+rem Prepare x86 binaries
 
 cd ..
 make all -j4 -B
 if ERRORLEVEL 1 exit 1
-
+strip wcm.exe
 cd install-windows
 
 rem Prepare SDK directory structure
@@ -22,8 +22,31 @@ copy ..\LICENSE Temp\LICENSE
 copy ..\readme_eng.txt Temp\readme_eng.txt
 xcopy ..\install-files\share\wcm Temp /S /Y
 
-"D:\Program Files (x86)\NSIS\makensis.exe" installer.nsi
+"D:\Program Files (x86)\NSIS\makensis.exe" installer32.nsi
 
 erase Temp /F /S /Q
 
-if not exist WalCommanderGitHub-0.16.2.exe exit 1
+if not exist WalCommanderGitHub-0.16.2-x86.exe exit 1
+
+rem Prepare x64 binaries
+
+cd ..
+make all -j4 -B WINDOWS_TARGET=-m64
+if ERRORLEVEL 1 exit 1
+strip wcm.exe
+cd install-windows
+
+rem Prepare SDK directory structure
+
+md Temp64
+copy ..\wcm.exe Temp64\wcm.exe
+copy ..\small.ico Temp64\small.ico
+copy ..\LICENSE Temp64\LICENSE
+copy ..\readme_eng.txt Temp64\readme_eng.txt
+xcopy ..\install-files\share\wcm Temp64 /S /Y
+
+"D:\Program Files (x86)\NSIS\makensis.exe" installer64.nsi
+
+erase Temp64 /F /S /Q
+
+if not exist WalCommanderGitHub-0.16.2-x64.exe exit 1
