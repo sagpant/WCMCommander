0b7109c33a7fc971a8a5ef5228a48397433ec492
Sergey Kosarevsky <sk@linderdaum.com>
2014-10-10 01:06:43 +0400

    Added defines to hide C-string functions

--- wcm/wal/types.h
+++ wcm/wal/types.h
@@ -3,3 +3,17 @@
 #include <stdint.h>
 
 #define CHECK_RET( cond, ret_val ) if ( !(cond) ) return (ret_val);
+
+#if _MSC_VER >= 1400
+#  define Lvsnprintf _vsnprintf_s
+#  define Lsnprintf  _snprintf_s
+#  define Lsscanf sscanf_s
+#  define Lwcsncpy wcsncpy_s
+#  define Lstrncpy strncpy_s
+#else
+#  define Lvsnprintf vsnprintf
+#  define Lsnprintf  snprintf
+#  define Lsscanf sscanf
+#  define Lwcsncpy wcsncpy
+#  define Lstrncpy strncpy
+#endif
