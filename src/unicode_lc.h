6761aefcf5076744076d93410d8f80dc50168699
Sergey Kosarevsky <sk@linderdaum.com>
2014-12-11 11:04:54 +0100

    Fixed MinGW compilation

--- src/unicode_lc.h
+++ src/unicode_lc.h
@@ -13,3 +13,20 @@ wal::unicode_t UnicodeLC( wal::unicode_t ch );
 
 /// convert a single UCS-2 character to uppercase
 wal::unicode_t UnicodeUC( wal::unicode_t ch );
+
+#if defined(__GNUC__) && defined(_WIN32)
+#include <string>
+
+// MinGW
+namespace std
+{
+	// dummy implementation
+	inline ::std::wstring to_wstring( uint64_t i )
+	{
+		wchar_t Buffer[255];
+		Buffer[0] = 0;
+		_ui64tow( i, Buffer, 10 );
+		return std::wstring( Buffer );
+	}
+}
+#endif // MinGW
