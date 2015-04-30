f2dce5fbbc6dffabdf2333fd140ee5931cad4b6a
Viktor Podzigun <viktor.podzigun@gmail.com>
2015-04-30 10:28:24 +0200

    Added clPluginFactory #26

--- src/plugin/plugin.cpp
+++ src/plugin/plugin.cpp
@@ -5,4 +5,27 @@
  */
 
 #include "plugin.h"
+#include "vfs.h"
 
+#include <map>
+#include <string>
+
+
+static std::map<std::string, clPluginFactory*> g_PluginRegistry;
+
+
+void clPluginFactory::Register( clPluginFactory* PluginFactory )
+{
+	g_PluginRegistry[std::string( PluginFactory->GetPluginId() )] = PluginFactory;
+}
+
+void clPluginFactory::Unregister( clPluginFactory* PluginFactory )
+{
+	g_PluginRegistry.erase( std::string( PluginFactory->GetPluginId() ) );
+}
+
+clPtr<FS> Plugin_OpenFileVFS( const unicode_t* FileExt, clPtr<FS> fs, FSPath& path )
+{
+	clPtr<FS> vfs;
+	return vfs;
+}
--- src/plugin/plugin.h
+++ src/plugin/plugin.h
@@ -6,3 +6,32 @@
 
 #pragma once
 
+#include "globals.h"
+
+
+/// Abstract plugin factory
+class clPluginFactory
+{
+private:
+
+	clPluginFactory( const clPluginFactory& ) = delete;
+	clPluginFactory& operator=(const clPluginFactory&) = delete;
+
+public:
+	virtual ~clPluginFactory() { }
+
+	static void Register( clPluginFactory* PluginFactory );
+	static void Unregister( clPluginFactory* PluginFactory );
+
+	/// Returns unique plugin id
+	virtual const char* GetPluginId() const = 0;
+};
+
+
+// plugin internal interface
+
+template <class T> class clPtr;
+class FS;
+class FSPath;
+
+clPtr<FS> Plugin_OpenFileVFS( const unicode_t* FileExt, clPtr<FS> fs, FSPath& path );
