78c8f37e3304fa3810f3e0b74ae8bb02fdaa2456
Thomas Klausner <wiz@NetBSD.org>
2015-09-08 15:18:57 +0200

    NetBSD does not provide gethostbyname_r().
    Handle it like the other BSDs that don't.

--- src/tcp_sock.cpp
+++ src/tcp_sock.cpp
@@ -64,7 +64,7 @@ bool GetHostIp( const char* utf8, unsigned* pIp, int* err )
 
 	struct hostent* ret;
 
-#if defined( __APPLE__ ) || defined(__OpenBSD__)
+#if defined( __APPLE__ ) || defined(__OpenBSD__) || defined(__NetBSD__)
 	ret = gethostbyname( utf8 );
 
 	if ( !ret )
